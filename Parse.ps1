92fe619995f8fdc5267678091ba5d3e00cabaf5b
Tadd E. Dawson <drtadd@hotmail.com>
2018-08-27 16:31:26 -0400

    With Minute Mile estimate for rate

--- Parse.ps1
+++ Parse.ps1
@@ -7,7 +7,7 @@ foreach ($Lap in $Game.TrainingCenterDatabase.Activities.Activity.Lap)
     $Lap | Select-Object StartTime, TotalTimeSeconds, DistanceMeters
     foreach($TrackPoint in $Lap.Track.Trackpoint)
     {
-        $entry = "" | Select-Object Time, Lat, Lon, TotalDistance, IncrementalDistance, Velocity, HeartRate
+        $entry = "" | Select-Object Time, Lat, Lon, TotalDistance, IncrementalDistance, Velocity, MinMile HeartRate
             $entry.Time = $TrackPoint.Time
             $entry.Lat = $TrackPoint.Position.LatitudeDegrees
             $entry.Lon = $TrackPoint.Position.LongitudeDegrees
@@ -16,13 +16,16 @@ foreach ($Lap in $Game.TrainingCenterDatabase.Activities.Activity.Lap)
             {
                 $entry.IncrementalDistance = 0
                 $entry.Velocity = 0
+                $entry.MinMile = 0
             }
             else 
             {
-                $entry.IncrementalDistance = $Entries[$Entries.Count-1].TotalDistance
-                $entry.Velocity = 0
+                $entry.IncrementalDistance = ($TrackPoint.DistanceMeters - $Entries[$Entries.Count-1].TotalDistance)
+                # m/s as the increment of th erecording is each second
+                # convert to min/mile to get a "feel" of the exertion
 
-                $Entries[$Entries.Count-1].TotalDistance
+                $entry.Velocity = $entry.IncrementalDistance
+                $entry.MinMile = $entry.IncrementalDistance * 10.3567
             }            
             
             $entry.HeartRate = $TrackPoint.HeartRateBpm.Value
